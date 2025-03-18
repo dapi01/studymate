15c3792ad1045a109662386cf241325c412eafe1
dap <ozalice011202@gmail.com>
2025-03-18 11:56:58 +0900

    feat : 모델 및 리포지토리 설정(회원가입처리용)

---
+++ src/main/java/org/codenova/studymate/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package org.codenova.studymate.repository;
+
+import lombok.AllArgsConstructor;
+import org.codenova.studymate.model.User;
+import org.mybatis.spring.SqlSessionTemplate;
+
+@AllArgsConstructor
+public class UserRepository {
+    private SqlSessionTemplate template;
+
+    public int create(User user) {
+        return template.insert("user.insert", user);
+    }
+    public User findById(String id) {
+        return template.selectOne("user.findById",id);
+    }
+}
